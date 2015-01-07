7b268f78a8c6ad396a106e7a1b93c98a215b644b
karthikbadam <sbadam@purdue.edu>
2015-01-06 22:11:22 -0500

    QR-Vis stocks

---
+++ js/LineChart.js
@@ -0,0 +1,115 @@
+/**
+ * Created by karthik on 1/6/15.
+ */
+
+function LineChart (options) {
+
+    var _self = this;
+
+    _self.data = options.data;
+    _self.symbol = options.symbol;
+
+    _self.margin = {
+        top: 60,
+        right: 40,
+        bottom: 60,
+        left: 40
+    };
+
+    _self.width = (800 - _self.margin.left - _self.margin.right),
+        _self.height = (680 - _self.margin.top - _self.margin.bottom);
+
+    _self.div = d3.select("body").append("div").attr("id", "chart_"+_self.symbol);
+
+    _self.svg = _self.div.append("svg")
+        .attr("width", _self.width + _self.margin.left - _self.margin.right)
+        .attr("height", _self.height + _self.margin.top + _self.margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + (_self.margin.left) + "," + _self.margin.top + ")");
+
+
+    _self.x = d3.time.scale()
+        .range([0, _self.width]);
+
+    _self.y = d3.scale.linear()
+        .range([_self.height, 0]);
+
+    _self.x.domain(d3.extent(_self.data, function(stock) {
+        return stock["Date"];
+    }));
+
+    _self.y.domain(d3.extent(_self.data, function(stock) {
+        return stock["Close"];
+    }));
+
+    //creates x and y axis
+    _self.xAxis = d3.svg.axis()
+        .scale(_self.x)
+        .orient("bottom").ticks(7)
+        .tickFormat(function(d) {
+            return d3.time.format('%b%d/%y')(new Date(d));
+        });
+
+    _self.yAxis = d3.svg.axis()
+        .scale(_self.y)
+        .orient("left").ticks(6);
+
+    //creates the mapping function for path line in SVG
+    _self.line = d3.svg.line()
+        .interpolate("Monotone")
+        .x(function(d) {
+            return _self.x(d["Date"]);
+        })
+        .y(function(d) {
+            return _self.y(d["Close"]);
+        });
+
+    //general definitions to keep everything within boundaries
+    _self.svg.append("defs")
+        .append("clipPath").attr("id", "clip-" + _self.symbol)
+        .append("rect")
+        .attr("width", _self.width).attr("height", _self.height);
+
+
+    //draws the path line
+    _self.chartContainer = _self.svg.append("g")
+        .attr("width", _self.width).attr("height", _self.height);
+
+    _self.chartContainer.append("path")
+        .attr("class", "line")
+        .attr("clip-path", "url(#clip-" + _self.symbol + ")")
+        .data([_self.data])
+        .attr("d", _self.line)
+        .attr("stroke", "#444")
+        .attr("fill", "transparent")
+        .attr("stroke-width", "2px")
+        .attr("z-index", 1);
+
+
+    //draws the axis
+    _self.chartContainer.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + _self.height + ")")
+        .call(_self.xAxis);
+
+    _self.chartContainer.append("g")
+        .attr("class", "y axis")
+        .call(_self.yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 4)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end");
+
+    //draws the text -- name of the stock
+    _self.chartContainer.append("text")
+        .attr("transform", "translate(" + 10 + ",0)")
+        .text(_self.symbol)
+        .attr("stroke-opacity", 0.3)
+        .attr("fill-opacity", 0.3)
+        .attr("stroke","#F00")
+        .attr("font-size", "11px");
+
+
+
+}
---
+++ js/main.js
@@ -0,0 +1,41 @@
+/**
+ * Created by karthik on 1/6/15.
+ */
+
+var googStock = "data/GOOG.csv";
+
+var closeValues = [];
+var keys = null;
+var parseDate = d3.time.format("%Y-%m-%d").parse;
+
+
+$(document).ready(function() {
+
+    d3.csv(googStock, function (error, data) {
+
+        //reading the data
+        data.forEach(function (d) {
+            if (keys == null) {
+                keys = Object.keys(d);
+            }
+            d["Date"] = parseDate(String(d["Date"]));
+            d["Close"] = +d["Close"];
+            d["Volume"] = +d["Volume"];
+
+            closeValues.push(d["Close"]);
+        });
+
+        //create a line chart
+        //creates a stock object for future reference
+        var chartObject = new LineChart({
+            data: data,
+            companyName: "Google",
+            symbol: "GOOG"
+        });
+
+    });
+
+
+
+});
+
